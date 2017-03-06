079cd03b1fdac99e755d05083156ffc265dd60f1
deadly8 <tarasent@ukr.net>
2017-03-06 12:49:19 +0200

    initial commit

---
+++ dist/js/script.js
@@ -0,0 +1,13 @@
+$("#example_id").ionRangeSlider({
+    type: "double",
+    grid: true,
+    min: 0,
+    max: 11
+});
+$("#price").ionRangeSlider({
+    type: "double",
+    grid: true,
+    min: 0,
+    max: 1000,
+    prefix: "$"
+});
